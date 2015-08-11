d20a03a4c7e056c7064d5bbbb4b708a46d5b9ee6
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-11 14:05:34 +0530

    Update TestFirstComeFirstServe.java

--- PF/Assignment1/TestFirstComeFirstServe.java
+++ PF/Assignment1/TestFirstComeFirstServe.java
@@ -29,43 +29,34 @@ public class TestFirstComeFirstServe {
 	@Test
 	public void test() {
 		 int ArrivalTime[] ={1, 5, 9, 25};
-		 int jobSize[]={12,7,2,5};
-		 int n=jobSize.length;
+		 int jobLength[]={12,7,2,5};
 		 FirstComeFirstServe assign=new FirstComeFirstServe();
-		 int output[][]= assign.FCFS(ArrivalTime, jobSize, n);
+		 int output[][]= assign.FCFS(ArrivalTime, jobLength);
 		 int expected[][]={{1,1,0,1,12},{2,5,8,13,19},{3,9,11,20,21},{4,25,0,25,29}};
-		 
-		/* System.out.println("job job arrival Time Job Waiting Time Job Start Time Job Finish Time");
-		
-		 for(int row=0;row<output.length;row++)
-		 {
-			 	for(int column=0;column<output[row].length;column++)
-				{
-			 		System.out.print(output[row][column]+"\t");
-				}
-			 	System.out.println();
-			 }*/
-		 int  ans=isArrayEqual(expected,output);
-		 assertEquals(1, ans);
+		 int  answer=0;
+		 boolean out=isArrayEqual(expected,output);
+		 if(out)
+			 answer=1;
+		 assertEquals(1, answer);
 	 }
 		
 		
-	int  isArrayEqual(int expected[][],int output[][])
+	boolean  isArrayEqual(int expected[][],int output[][])
 	{
-		int flag=1;
+		boolean isArrayEqual=false;
 		for(int row=0;row<expected.length;row++)
 		{
 			for(int column=0;column<expected[row].length;column++)
 			{
 				if(expected[row][column]!=output[row][column])
 				{
-					flag=0;
-					return flag;
+					isArrayEqual=true;
+					return isArrayEqual;
 					
 				}
 			}
 		}
-		return flag;
+		return isArrayEqual;
 	}
 	
 
