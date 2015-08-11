3aed51b7254cd8adebff15c47332a26e5d10a0f5
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-11 14:03:33 +0530

    Update FirstComeFirstServe.java

--- PF/Assignment1/FirstComeFirstServe.java
+++ PF/Assignment1/FirstComeFirstServe.java
@@ -1,49 +1,106 @@
+/**
+4
+ * Program to Schedule the required jobs in First Come First Serve manner
+ * Signature used int[][]FCFS(int arrivalTime,int jobLength)
+ */
 package FirstComeFirstServe;
-
+import java.util.*;
 public class FirstComeFirstServe{
-	 int[][] FCFS( int arrivalTime [], int jobSize[] ,int n)
-	 {
-		 int i;
-		 int jobStartTime[]=new int[n];
-		 int jobWaitTime[]=new int[n];
-		 int jobFinishTime[]=new int[n];
-		 jobStartTime[0]=1;
-		 jobWaitTime[0]=0;
-		 jobFinishTime[0]=jobSize[0];
-		 for(i=1;i<n;i++)
-		 {
-			 if(arrivalTime[i]<jobFinishTime[i-1])
-			 {
-			 jobStartTime[i]=jobFinishTime[i-1]+1;
-			 jobWaitTime[i]=jobStartTime[i]-arrivalTime[i];
-			 jobFinishTime[i]=jobStartTime[i]+jobSize[i]-1;
-			 }
-			 else
-			 {
-				 jobWaitTime[i]=0;
-				 jobStartTime[i]=arrivalTime[i];
-				 jobFinishTime[i]=jobStartTime[i]+jobSize[i]-1;
-				 
-			 }
-			 
-		 }
-		 int output[][]=new int[n][5];
-		 for(i=0;i<n;i++)
-		 {
-			 
-			
-				 output[i][0]=i+1;
-				 output[i][1]=arrivalTime[i];
-				 output[i][2]=jobWaitTime[i];
-				 output[i][3]=jobStartTime[i];
-				 output[i][4]=jobFinishTime[i];
-			 
-			 
-		 }
-		 return output;
-	 }
-	 public static void main(String arg[])
-	 {
-	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirstComeFirstServe firstComeFirstServe=new FirstComeFirstServe();
+		Scanner sc=new Scanner(System.in);
+		int noOfJobs;
+		System.out.println("Enter No of Jobs: ");
+		noOfJobs=sc.nextInt();
+		int arrivalTime[]=new int[noOfJobs];
+		int jobLength[]=new int[noOfJobs];
+		System.out.println("Enter Arrival Times: ");
+		for(int index=0;index<arrivalTime.length;index++)
+		{
+			arrivalTime[index]=sc.nextInt();
+		}
+		System.out.println("Enter Job Size: ");
+		for(int index=0;index<jobLength.length;index++)
+		{
+			jobLength[index]=sc.nextInt();
+		}
+		int output[][]=firstComeFirstServe.FCFS(arrivalTime,jobLength);
+		System.out.print("job\t"+"job arrived time\t"+"job wait time\t"+"job start at\t"+"job finished at\n");
+		for(int row=0;row<arrivalTime.length;row++)
+		{
+			for(int column=0;column<5;column++)
+			{
+				System.out.print(output[row][column]+"\t\t");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+	
+	public int[][] FCFS(int arrivalTime[], int jobLength[])
+	{
+		int temp;
+		int rowLength=arrivalTime.length;
+		int jobs[] = new int[rowLength];
+		int waitingTime[] = new int[rowLength];
+		int jobFinishTime[] =new int[rowLength];
+		int jobStartTime[] =new int[rowLength];
+		/*For initialize job number like 1,2,3,4 */
+		for(int index=0;index<rowLength;index++)
+		{
+			jobs[index]=index+1;
+		}
+		/* For Order in ascending of arrivalTime*/
+		for(int row=0;row<rowLength;row++)
+		{
+			for(int column=row;column<rowLength;column++)
+			{
+				if(arrivalTime[row]>arrivalTime[column])
+				{
+					temp=arrivalTime[row];
+					arrivalTime[row]=arrivalTime[column];
+					arrivalTime[column]=temp;
+				}
+			}
+		}
+		/*Calculate waiting time*/
+		waitingTime[0]=0;
+		int size=jobLength[0];
+		for(int index=1;index<rowLength;index++)
+		{
+			waitingTime[index]=size-arrivalTime[index]+1;
+			if(waitingTime[index]<0)
+			{
+				waitingTime[index]=0;
+			}
+			size+=jobLength[index];
+		}
+		/*Calculate job finished time*/
+		jobFinishTime[0]=jobLength[0]+arrivalTime[0];
+		for(int index=1;index<rowLength;index++)
+		{
+			jobFinishTime[index]=((jobFinishTime[index-1]>arrivalTime[index])?jobFinishTime[index-1]:arrivalTime[index]-1)+jobLength[index];
+		}
+		jobStartTime[0]=(arrivalTime[0]>1)?arrivalTime[0]:1;
+		/*Calculate job Start Time*/
+		for(int index=1;index<rowLength;index++)
+		{
+			jobStartTime[index]=jobStartTime[index-1]+jobLength[index-1];
+			jobStartTime[index]=(jobStartTime[index]>=arrivalTime[index])?jobStartTime[index]:arrivalTime[index];
+		}
+		/*Store the result in 2 dimensional array*/
+		int[][] result=new int[rowLength][5];
+		for(int index=0;index<rowLength;index++)
+		{
+			result[index][0]=jobs[index];
+			result[index][1]=arrivalTime[index];
+			result[index][2]=waitingTime[index];
+			result[index][3]=jobStartTime[index];
+			result[index][4]=jobFinishTime[index];
+		}
+		return result;
+	}
 
 }
