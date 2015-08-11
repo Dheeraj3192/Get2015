92d0bd4cb016e9a0b7c18515b3efe0d3762e51de
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-11 14:14:36 +0530

    Update LongestSequence.java

--- PF/Assignment1/LongestSequence.java
+++ PF/Assignment1/LongestSequence.java
@@ -6,6 +6,13 @@ public class LongestSequence {
 	
 		public static void main(String[] args) 
 		{
+			LongestSequence obj=new LongestSequence();
+			int input[]={1,2,3,4,5,2,3,4,7,8,9,1,2,3,3,3,4,5,6,7,8,9};
+			int output[]=obj.longestSequence(input);
+			for(int index=0;index<output.length;index++)
+				System.out.println(output[index]);
+		
+		assertArrayEquals(expected, actual);
 			
 		}
 		int[] longestSequence(int input[])
