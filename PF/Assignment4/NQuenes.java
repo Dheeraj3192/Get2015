cb131cc23faa0854e868091b4f4b8e6eca04c5c0
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-08-13 17:30:41 +0530

    Create NQuenes.java

---
+++ PF/Assignment4/NQuenes.java
@@ -0,0 +1,104 @@
+/**
+ * @author Dheeraj
+ *Program to Solve the N queens problem where N>1
+ */
+/*Starting of NQueens Class*/
+public class NQuenes {
+	
+	/*Starting of main method*/
+	public static void main(String[] args) 
+	{
+		NQuenes nQueens =new NQuenes();
+		nQueens.solveNQueens(4);
+	}
+	/*Ending of main method*/
+	
+	/*Printing the solution*/
+	boolean[][] getBoard(boolean board[][])
+	{
+		
+		return board;
+	   
+	}
+	/*Check that queens place is safe at all direction*/
+	boolean isSafe(boolean board[][], int row, int column)
+	{
+	 
+		/* Check the row on left side */
+	    for (int index = 0; index < column; index++)
+	    {
+	        if (board[row][index])
+	            return false;
+	    }
+	 
+	    /* Check upper diagonal left side */
+	    for (int index1 = row, index2 = column; index1 >= 0 && index2 >= 0; index1--, index2--)
+	    {
+	        if (board[index1][index2])
+	            return false;
+	    }
+	    /* Check lower  diagonal on left side */
+	    for (int index1 = row, index2 = column; index2 >= 0 && index1 < board.length; index1++, index2--)
+	    {
+	        if (board[index1][index2])
+	            return false;
+	    }
+	    /*if the queen is safe */
+	    return true;
+
+	}
+	boolean solveNQueenProblem(boolean board[][], int column)
+	{
+	    /*  If all queens are placed then return true */
+	    if (column >= board.length)
+	        return true;
+	 
+	    /* Consider this column and try place queen in all rows
+	       one by one */
+	    for (int index = 0; index < board.length; index++)
+	    {
+	        /* Check if queen can be placed on board[i][column] */
+	        if ( isSafe(board, index, column) )
+	        {
+	            /* Place this queen in board[i][column] */
+	            board[index][column] = true;
+	 
+	            /*Place rest of the queen */
+	            if ( solveNQueenProblem(board, column + 1) == true )
+	                return true;
+	 
+	            /* If placing queen in board[i][column] then remove queen from board[i][column] */
+	            board[index][column] = false; // Backtracking
+	        }
+	    }
+	 
+	     /* If queen can not be place then return false */
+	    return false;
+	}
+	/*Starting of the NQueens problem */
+	boolean[][] solveNQueens(int noOfQueens)
+	{
+	    boolean board[][] = new boolean[noOfQueens][noOfQueens];
+	    if ( solveNQueenProblem(board, 0) == false )
+	    {
+	      System.out.println("Solution does not exist");
+	      System.exit(1);
+	    }
+	  //  printSolution(board);
+	    //return true;
+	    return board;
+	}
+	
+}
+/*Ending of NQueens Class*/	 
+
+ 
+
+   
+
+ 
+   
+   
+
+
+
