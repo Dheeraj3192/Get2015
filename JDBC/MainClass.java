f9c89d8ca7f39195e17bc398fb68f5f4af826063
Dheeraj3192 <dheeraj.kumar1@metacube.com>
2015-09-19 11:54:30 +0530

    Update MainClass.java

--- JDBC/MainClass.java
+++ JDBC/MainClass.java
@@ -1,5 +1,8 @@
 package com.metacube.jdbc.session1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class MainClass
@@ -8,23 +11,51 @@ public class MainClass
 	public static void main(String[] args) 
 	{
 		Helper helper = new Helper();
-		String authorName = "Sandeep Sopankar";
+		System.out.println("Enter author name");
+		String authorName =getString();
 		List<BookTitles> listOfTitles =helper.titlesByAuthor(authorName.trim());
-		System.out.println("List of Titles of author "+authorName+ " are Following....");
-		for(BookTitles  bookTitles: listOfTitles)
-		{
-			System.out.println("\t"+bookTitles);
+		if(listOfTitles.size()==0)
+			System.out.println("No  record available");
+		else {
+			System.out.println("List of Titles of author "+authorName+ " are Following....");
+			for(BookTitles  bookTitles: listOfTitles)
+				System.out.println("\t"+bookTitles);
 		}
-		String bookName="AI- A Practicle Approach";
-		String memberName ="Girdhari";
-		String output="Yes";
+		System.out.println("Enter book name");	
+		String bookName=getString();
+		System.out.println("Enter name who book issue");
+		String memberName =getString();
 		boolean hasIssued =helper.hasBookIssued(bookName, memberName);
 		if(!hasIssued)
-			output="No";
-		System.out.println("\nBook "+bookName+" Issued to "+memberName +" "+output);
+			System.out.println("No record available");
+		else 
+			System.out.println("\nBook "+bookName+" Issued to "+memberName );
 		int noOfBooks =helper.deleteBooksSinceIssueNotLastYear();
 		System.out.println("\nNo of Books Deleted is since last year "+noOfBooks);
 		System.out.println("\nExiting . . .");
 	}
 
+	
+	
+	public static String getString () {
+		String input="";
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			try {
+				input = bufferedReader.readLine().trim();
+				
+			}
+			catch(IOException io) {
+				System.out.println("Please enter input again ");
+				continue;
+			}
+			if(input.equals("")) {
+				System.out.println("Please enter valid input");
+				continue;
+			}
+			break;
+		}while(true);
+		
+		return input;
+	}
 }
