d15081eaf5d467123f989329b051c12479813ebb
Dheeraj Kumar <dheeraj.kumar1@metacube.com>
2015-09-28 20:00:14 +0530

    html 3 completed

---
+++ Web/HTML-Assignment-3/js/script1.js
@@ -0,0 +1,11 @@
+
+window.onload = function() {
+	
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	<!-- Fill Style used-->
+	ctx.fillStyle = "#FFF000";
+	<!-- Rectangle Filling Is used-->
+	ctx.fillRect(20, 20, 250, 100);
+
+}
